c472ef8db870c90dc423166450d3f1f52774b54d
hiroji <joegilbertdev@outlook.com>
2026-04-10 18:25:46 +0100

    Created a dictionary of dozahzul to english translations

--- src/lib/utils/convert-csv-dictionary-to-json.ts
+++ src/lib/utils/convert-csv-dictionary-to-json.ts
@@ -1,10 +1,16 @@
 import { readFile, writeFile } from 'node:fs';
 import * as path from 'node:path';
+import { reverseObject } from './reverse-object';
 
 // Converts csv file of english dovahzul translations into a json file.
 // This function should not be required once the json has been created.
-const translateCsvDictToJson = () => {
+// Pass shouldReverse true to create a dictionary of Dovahzul to English translations and false for English to Dovahzul translations
+const translateCsvDictToJson = (shouldReverse: boolean) => {
 	console.log('Converting csv to a json file.');
+	const writeFilePath = path.resolve(
+		`../../../data/${shouldReverse ? 'dovahzul-english' : 'english-dovahzul'}.json`
+	);
+
 	const filepath = path.resolve('../../../data/english-dovahzul.csv');
 
 	readFile(filepath, 'utf-8', (err, data) => {
@@ -12,7 +18,6 @@ const translateCsvDictToJson = () => {
 			console.error(`An error occurred reading the csv: ${err}`);
 		} else {
 			const translationRows = data.split('\n').slice(1);
-
 			const translationObj: Record<string, string[]> = {};
 
 			translationRows.forEach((row) => {
@@ -23,14 +28,32 @@ const translateCsvDictToJson = () => {
 				translationObj[key] = value;
 			});
 
-			const writeFilePath = path.resolve('../../../data/english-dovahzul.json');
-
-			writeFile(writeFilePath, JSON.stringify(translationObj, null, 2), 'utf8', (err) => {
-				if (err)
-					console.error(`An error occurred writing the translation object to a json file: ${err}`);
-			});
+			// Dovahzul to english dictionary
+			if (shouldReverse) {
+				const reverseTranslationObj = reverseObject(translationObj);
+				console.log(writeFilePath);
+				console.log(JSON.stringify(reverseTranslationObj));
+				writeFile(writeFilePath, JSON.stringify(reverseTranslationObj, null, 2), 'utf8', (err) => {
+					if (err)
+						console.error(
+							`An error occurred writing the translation object to a json file: ${err}`
+						);
+				});
+			}
+			// English to Dovahzul dictionary
+			else {
+				writeFile(writeFilePath, JSON.stringify(translationObj, null, 2), 'utf8', (err) => {
+					if (err)
+						console.error(
+							`An error occurred writing the translation object to a json file: ${err}`
+						);
+				});
+			}
 		}
+
 		console.log('Finished!');
 	});
 };
-translateCsvDictToJson();
+
+translateCsvDictToJson(false);
+translateCsvDictToJson(true);
