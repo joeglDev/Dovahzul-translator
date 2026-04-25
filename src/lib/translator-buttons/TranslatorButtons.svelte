def9f15303faf39e9f7d3ed6a9c850d9e04e7ac9
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2026-04-25 10:13:09 +0100

    Feat/clear text button (#8)
    * Added 'Clear text' button. When clicked input and translated text is cleared. Moved buttons into a new component. Lint now does not run prettier
    
    * Added playwright test that input text can be cleared by pressing the clear button.

---
+++ src/lib/translator-buttons/TranslatorButtons.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import PrintButton from './PrintButton.svelte';
+	import type { TranslatorButtonsProps } from './TranslatorButtons.types';
+
+	const { inputText, translatedText, onClearText }: TranslatorButtonsProps = $props();
+</script>
+
+<div class="container">
+	<button onclick={onClearText}>Clear text</button>
+	<PrintButton {inputText} {translatedText} />
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+		justify-content: center;
+	}
+
+	button {
+		margin: 1rem 0;
+	}
+</style>
